1230e3a20b19a09ea3feab5ee2202b1fbc77f1cd
Aline Santos <Aline Santos>
2016-12-06 11:04:49 -0200

    inclusao de arwuivos faltantes no commit anterior - hsql

--- src/main/java/br/com/cs/desafio/application/Application.java
+++ src/main/java/br/com/cs/desafio/application/Application.java
@@ -2,19 +2,16 @@ package br.com.cs.desafio.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 // @EnableAutoConfiguration
-@ComponentScan("br.com.cs.desafio.**")
-@EnableJpaRepositories(basePackages = { "br.com.cs.desafio.dao.**" })
-@EntityScan(basePackages = { "br.com.cs.*" })
-@PropertySource("application.properties")
+//@ComponentScan("br.com.cs.desafio.**")
+//@EnableJpaRepositories(basePackages = { "br.com.cs.desafio.dao.**" })
+//@EntityScan(basePackages = { "br.com.cs.*" })
+@PropertySource("config/application.properties")
 public class Application extends SpringBootServletInitializer {
 
 	@Override
---
+++ src/main/resources/data/hsqldb/data.sql
@@ -0,0 +1,5 @@
+Insert into Customer (UUID, Created_Timestamp, Active, Email, Password, Last_Login, Modified, Token, Name)
+values('2sdhhgfdshbaldb', CURDATE(), 1, 'joao@gmail.com', 'password', CURDATE(), CURDATE(), null, 'João');
+
+Insert into Customer (UUID, Created_Timestamp, Active, Email, Password, Last_Login, Modified, Token, Name)
+values('23456dbjhgfdsdf', CURDATE(), 1, 'maria@gmail.com', 'password', CURDATE(), CURDATE(), null, 'Maria');
